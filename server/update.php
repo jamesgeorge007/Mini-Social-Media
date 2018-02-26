a912672f2c41a9c70847524940da1b6550efa36f
James George <jamesgeorge998001@gmail.com>
2018-02-26 18:14:34 +0530

    UI improvements.

--- server/update.php
+++ server/update.php
@@ -1,21 +1,29 @@
 <?php
 session_start();
-$username=@$_POST['username'];
-$first=@$_POST['firstname'];
-$last=@$_POST['lastname'];
-$email=@$_POST['email'];
+
 if(@$_SESSION['user']!="")
 {
+
+  $first=$_POST['firstname'];
+  $last=$_POST['lastname'];
+  $email=$_POST['email'];
+
  $conn=mysqli_connect("localhost","root","","login");
- $query1="UPDATE credentials SET username='$username' WHERE username='".$_SESSION['user']."'";
- $query2="UPDATE credentials SET firstname='$first' WHERE username='".$_SESSION['user']."'";
- $query3="UPDATE credentials SET lastname='$last' WHERE username='".$_SESSION['user']."'";
- $query4="UPDATE credentials SET email='$email' WHERE username='".$_SESSION['user']."'";
+
+
+ $query1="UPDATE credentials SET firstname = {$first} WHERE username = {$_SESSION['user']}";
+ 
+ 
+ $query2="UPDATE credentials SET lastname = {$last} WHERE username = {$_SESSION['user']}";
+ 
+
+ $query3="UPDATE credentials SET email = {$email} WHERE username = {$_SESSION['user']}";
+
  mysqli_query($conn,$query1);
  mysqli_query($conn,$query2);
  mysqli_query($conn,$query3);
- mysqli_query($conn,$query4);
- $_SESSION['user']=$username;
+ 
+
  mysqli_close($conn);
  header('location:viewprofile.php');
 }
