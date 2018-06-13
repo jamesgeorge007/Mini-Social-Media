711d4b9a6a9a12f77c617f133b77427e220d6236
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-06-13 21:59:49 +0530

    Done with dashboard styling

--- Mini-Social-Media/resources/views/dashboard.blade.php
+++ Mini-Social-Media/resources/views/dashboard.blade.php
@@ -1,1 +1,45 @@
-<h1>The Dashboard</h1>
+@extends('layouts.index')
+
+@section('content')
+    <section class="row new-post">
+         <div class=".col-md-6 col-md-offset-3">
+            <header><h3>What do you have to say?</h3></header>
+            <form action="#">
+                <div class="form-group">
+                    <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Your Post"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Create Post</button>
+            </form>
+         </div>
+    </section>
+
+    <section class="row posts">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What other people say...</h3></header>
+            <article class="post">
+                <p>Some text here.</p>
+                <div class="info">
+                    Posted by Random guy.
+                </div>
+                <div class="interactions">
+                    <a href="#">Like</a>
+                    <a href="#">DisLike</a>
+                    <a href="#">Edit</a>
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+            <article class="post">
+                <p>Some text here.</p>
+                <div class="info">
+                    Posted by Random guy.
+                </div>
+                <div class="interactions">
+                    <a href="#">Like</a>
+                    <a href="#">DisLike</a>
+                    <a href="#">Edit</a>
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+        </div>
+    </section>
+@endsection
--- Mini-Social-Media/resources/views/layouts/index.blade.php
+++ Mini-Social-Media/resources/views/layouts/index.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ URL::to('css/dashboard.css') }}">
 </head>
 <body>
     @include('includes.header')
