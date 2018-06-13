4ffa9051a0aafd3c1ac9751c952326e42cb651d5
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-06-13 17:44:05 +0530

    Add validations for the form fields in the required way using Laravel validate method

--- Mini-Social-Media/resources/views/welcome.blade.php
+++ Mini-Social-Media/resources/views/welcome.blade.php
@@ -7,7 +7,7 @@ Welcome
 @section('content')
     @if(count($errors) > 0)
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-4 col-md-offset-4">
             <ul>
                 @foreach($errors->all() as $error)
                     <li>{{ $error }}</li>
@@ -20,17 +20,17 @@ Welcome
         <div class="col-md-6">
         <h3>Sign Up</h3>
             <form method="post" action="{{ route('signup') }}">
-               <div class="form-group"> 
+               <div class="form-group {{ $errors->has('email')? 'has-error':'' }}"> 
                 <label for="email">E-mail</label>
-                <input type="email" class="form-control" name="email">
+                <input type="email" class="form-control" name="email" value="{{ Request::old('email') }}">
                </div>
-               <div class="form-group"> 
+               <div class="form-group {{ $errors->has('first_name')? 'has-error':'' }}"> 
                 <label for="first_name">First Name</label>
-                <input type="text" class="form-control" name="first_name">
+                <input type="text" class="form-control" name="first_name" value="{{ Request::old('first_name') }}">
                </div>
-               <div class="form-group"> 
+               <div class="form-group {{ $errors->has('password')? 'has-error':'' }}"> 
                 <label for="password">Password</label>
-                <input type="password" class="form-control" name="password">
+                <input type="password" class="form-control" name="password" value="{{ Request::old('password') }}">
                </div> 
                <button type="submit" class="btn btn-success">Submit</button>
                <input type="hidden" name="_token" value="{{ Session::token() }}">
@@ -40,13 +40,13 @@ Welcome
         <div class="col-md-6">
             <h3>Sign In</h3>
             <form method="post" action="{{ route('signin') }}">
-            <div class="form-group"> 
+            <div class="form-group {{ $errors->has('email')? 'has-error':'' }}"> 
                 <label for="email">E-mail</label>
-                <input type="text" class="form-control" name="email">
+                <input type="text" class="form-control" name="email" value="{{ Request::old('email') }}">
                </div>
-               <div class="form-group"> 
+               <div class="form-group {{ $errors->has('password')? 'has-error':'' }}"> 
                 <label for="password">Password</label>
-                <input type="password" class="form-control" name="password">
+                <input type="password" class="form-control" name="password" value="{{ Request::old('password') }}">
                </div> 
                <button type="submit" class="btn btn-primary">Submit</button>
                <input type="hidden" name="_token" value="{{ Session::token() }}">
