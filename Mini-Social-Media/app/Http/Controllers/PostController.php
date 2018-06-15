73a7e1b8ab944ae4616ca168434fead4493086d6
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-06-15 18:05:50 +0530

    Add validations for creating new posts.

--- Mini-Social-Media/app/Http/Controllers/PostController.php
+++ Mini-Social-Media/app/Http/Controllers/PostController.php
@@ -8,9 +8,15 @@ class PostController extends Controller
 {
     public function CreatePost(Request $request)
     {
+        $this->validate($request, [
+            'new_post' => 'required|max:1000'
+        ]);
         $post = new Post();
         $post->body = $request['new_post'];
-        $request->user()->posts()->save($post);
-        return redirect()->route('dashboard');
+        $message = "Something went wrong";
+        if($request->user()->posts()->save($post)) {
+            $message = "Successfully posted!";
+        } 
+        return redirect()->route('dashboard')->with(['message' => $message]);
     }
 }
--- Mini-Social-Media/resources/views/dashboard.blade.php
+++ Mini-Social-Media/resources/views/dashboard.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.index')
 
 @section('content')
+@include('includes.message-block')
     <section class="row new-post">
          <div class=".col-md-6 col-md-offset-3">
             <header><h3>What do you have to say?</h3></header>
